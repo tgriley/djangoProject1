ac5da9520b4218fda740446f84057e612b3276e2
tgriley <tgriley@gmail.com>
2024-04-14 11:50:03 +0100

    Removed failing test

--- app/tests.py
+++ app/tests.py
@@ -2,11 +2,6 @@ from django.test import TestCase
 
 
 # Create your tests here.
-class PassingTest(TestCase):
+class Test(TestCase):
     def test(self):
         self.assertTrue(True)
-
-
-class FailingTest(TestCase):
-    def test(self):
-        self.assertTrue(False)
